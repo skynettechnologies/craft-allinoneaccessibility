af50fcfdeed71c53fce43952ce04415531f7c5a8
Skynet Technologies <developer3@skynettechnologies.com>
2026-09-17 18:28:02 +0530

    Solved minor issues

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,17 @@
 # Changelog
 
-## 2.0.0 
+## 3.0.0 
 ### Added
 
 - Initial release
+
+## 3.0.1 
+### Added
+
+- Readme Content Update
+
+## 3.0.2 
+### Added
+
+- Code Commenting
+
--- src/models/Settings.php
+++ src/models/Settings.php
@@ -1,25 +1,26 @@
-<?php
-
-namespace skynettechnologies\craftAllinoneaccessibility\models;
-use craft\base\Model;
-
-class Settings extends Model
-{
-  public $license_key = "";
-  public $color = "#600b96";
-  public $position = "bottom_right";
-  public $icon_type = "aioa-icon-type-1";
-  public $icon_size = "aioa-medium-icon";
-  public $isvalid_key = 0;
-
-  public function rules(): array
-  {
-      return [
-        // these attributes are required
-        [['color', 'position', 'icon_type', 'icon_size'], 'required', 'message' => 'Please complete all required fields']
-      ];
-  }
-  
-}
-
-
+<?php
+
+namespace skynettechnologies\craftAllinoneaccessibility\models;
+use craft\base\Model;
+
+class Settings extends Model
+{
+  public $license_key = "";
+  public $color = "#600b96";
+  public $position = "bottom_right";
+  public $icon_type = "aioa-icon-type-1";
+  public $icon_size = "aioa-medium-icon";
+  public $isvalid_key = 0;
+
+  public function rules(): array
+  {
+
+      return [
+        // these attributes are required
+        [['color', 'position', 'icon_type', 'icon_size'], 'required', 'message' => 'Please complete all required fields']
+      ];
+  }
+  
+}
+
+
